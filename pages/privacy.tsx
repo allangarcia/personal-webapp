a76793ab364d9d4d39e370951b4f21b4e0d9cfe0
Allan Garcia <allan.garcia@gmail.com>
2022-08-11 10:25:23 -0300

    Fixing a bug on privacy page

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -6,17 +6,25 @@ export const Privacy: NextPage = () => {
   return (
     <div className="bg-zinc-800">
       <Head>
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
         <title>Hi, this is Allan Garcia speaking! - Privacy Policy</title>
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
+
+      {/* begin header */}
+      <div className="p-5 bg-sky-900 bg-opacity-20">
+        <Link href="/">
+          <a>
+            <h6 className="text-lg text-sky-500 text-opacity-80 font-mono sm:text-xl hover:text-orange-400">
+              <i className="fa fa-house" aria-label="Back to homepage"></i> Home
+            </h6>
+          </a>
+        </Link>
+      </div>
+      {/* end header */}
+
+      {/* begin content */}
       <div className="p-5 flex flex-col flex-shrink md:min-h-screen">
         <article className="text-justify">
           <h2 className="py-6 text-3xl text-orange-400 font-semibold md:text-4xl">
@@ -77,81 +85,7 @@ export const Privacy: NextPage = () => {
           </Link>
         </article>
       </div>
-      <div className="p-5 bg-sky-900 bg-opacity-20">
-        <footer className="flex flex-row justify-between items-center md:container md:mx-auto">
-          <h6 className="text-xs text-sky-500 text-opacity-80 font-semibold">
-            <i className="far fa-copyright"></i> Copyright 2022
-          </h6>
-          <ul className="flex flex-col text-right md:flex-row gap-4 md:gap-12 text-xs text-sky-500 text-opacity-80 text-semibold font-mono">
-            <li>
-              <a
-                className="hover:text-orange-400"
-                href="https://www.linkedin.com/in/allangarcia/"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="no-referrer link to LinkedIn."
-              >
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <a
-                className="hover:text-orange-400"
-                href="https://github.com/allangarcia"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="no-referrer link to Git-Hub."
-              >
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a
-                className="hover:text-orange-400"
-                href="https://www.twitter.com/AllanGarciaDev"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="no-referrer link to Twitter."
-              >
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a
-                className="hover:text-orange-400"
-                href="https://apps.apple.com/br/developer/allan-garcia/id1005147963"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="no-referrer link to App Store Developer Page."
-              >
-                AppStore
-              </a>
-            </li>
-            <li>
-              <a
-                className="hover:text-orange-400"
-                href="https://www.youtube.com/channel/UCcV6qHiwx_9clQzWMbqHLqA"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="no-referrer link to You-Tube."
-              >
-                YouTube
-              </a>
-            </li>
-            <li>
-              <Link href="/privacy">
-                <a
-                  className="hover:text-orange-400"
-                  rel="privacy policy"
-                  aria-label="Jump to Privacy Policy"
-                >
-                  Privacy Policy
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </footer>
-      </div>
+      {/* end content */}
     </div>
   )
 }
